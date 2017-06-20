b1dc0985fa2962eb3ab8adc77de2af929315703a
Toma Marius Gimi <gimi10mail@gmail.com>
2017-06-20 11:14:43 +0300

    Changed the app title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    WEMOSAPP
                 </div>
 
                 <div class="links">
